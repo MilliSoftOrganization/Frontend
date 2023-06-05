78a8fe164544c88fea11e3debf8952ab3a0b0bed
Natiq Haciyev <100693634+natiqhaciyef@users.noreply.github.com>
2023-06-05 17:32:54 +0400

    Delete Millisoft directory

--- Millisoft/indenJs/index.js
+++
@@ -1,9 +0,0 @@
-// $("#toggle").click(function () {
-//   $(this).toggleClass("active");
-// });
-
-document.querySelector("#toggle").addEventListener("click",function(){
-    this.classList.toggle("active");
-    document.querySelector("nav").classList.toggle("block")
-    document.querySelector("nav").classList.toggle("top");
-})
